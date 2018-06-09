45b42cba8baef3515dc3d72a29e378287493f2f8
RongfengCui <rongfengcui123@gmail.com>
2018-06-08 21:20:00 -0300

    stage2  add junit test

--- app/src/test/java/ca/dal/csci3130/csci3130ass2/ValidatorTest.java
+++ app/src/test/java/ca/dal/csci3130/csci3130ass2/ValidatorTest.java
@@ -5,24 +5,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ValidatorTest {
-    //Test Stage1
+    //Test Stage2
     @Test
-    public void validatorTestStage1() {
-        //following two rules
-        //it is not "password" (case insensitive)
-        //it is at least 8 characters long
+    public void validatorTestStage2() {
+        //following five rules
+        //rule 1: it is not "password" (case insensitive)
+        //rule 2: it is at least 8 characters long
+        //rule 3: it is at least 1 special character(which have to be @ ? !)
+        //rule 4: it is at least 1 digit
+        //rule 5: it contains both upper case and lower case
 
-        //test if user doesn't type any password(satisfy 0 rule)
-        assertEquals(0, Validator.Stage1(""));
+        //test if password satisfies rule 1,2,3,4,5
+        assertEquals(5,Validator.Stage2("Pass123!"));
 
-        //test if user type a "password" in any case(satisfy 1 rule)
-        assertEquals(1, Validator.Stage1("Password"));
+        //test if password satisfies rule 1,2,4,5
+        assertEquals(4,Validator.Stage2("Ab1232434"));
 
-        //test if user type a word which has less than 8 characters(satisfy 1 rule)
-        assertEquals(1, Validator.Stage1("pass"));
+        //test if password satisfies rule 1,2,3,5
+        assertEquals(4,Validator.Stage2("Abcdefg!!!!"));
 
-        //test if user type a word which has more than or equal 8 characters and not equal "password"(satisfy 2 rules)
-        assertEquals(2, Validator.Stage1("abcdefgh"));
-        assertEquals(2, Validator.Stage1("aaaaaaaaaaaaaa123"));
+        //test if password satisfies rule 1,2,5
+        assertEquals(3,Validator.Stage2("Youaregreat"));
+
+        //test if password satisfies rule 1,4,5
+        assertEquals(3,Validator.Stage2("You123"));
+
+        //test if password satisfies rule 1,2
+        assertEquals(2,Validator.Stage2("banananana"));
+
+        //test if password satisfies rule 1
+        assertEquals(1,Validator.Stage2("apple"));
     }
 }
